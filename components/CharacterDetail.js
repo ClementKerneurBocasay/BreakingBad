f2a9d4bd38ad5b5d843340ac132f6d8f2fb6e066
Rajnish Gowreesunker <rajnishgowreesunker@outlook.com>
2021-11-25 17:43:57 +0400

    Bassic Character Detail Screen now working
    Fed with Dummy Data

--- components/CharacterDetail.js
+++ components/CharacterDetail.js
@@ -1,23 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+/** URL Tutorial : https://www.youtube.com/watch?v=xVOT0cxFhzA */
 
 export default class CharacterDetail extends React.Component {
  render() {  
         return (
             <View style={styles.container}>
-              <Text>Oi! character details !!</Text>
-              <StatusBar style="auto" />
+              
+              <View style = {styles.detailsContainer}>
+                <Text style = {styles.title}> Title </Text>
+                <Text style = {styles.subtitle} > subti </Text>
+                <Text style = {styles.description} > zafer lap marser !! </Text>
+              </View>
+
+              <View style = {styles.speakerContainer}>
+                <Image
+                style = {styles.image}
+                source = {{uri: "https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441"}}
+                />
+
+                <View>
+                  <Text style = {styles.speakerName}> Breaking Bad </Text>
+                  <Text style = {styles.speakerBio}> is Working haha</Text>
+                </View>
+              </View>
+
             </View>
           );
     } 
 }
 
+const IMAGE_SIZE = 80;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+
+  detailsContainer: {
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "black"
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+    marginBottom: 4
+  },
+
+  subtitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 12
+  },
+
+  description: {
+    fontSize: 13
+  },
+
+  speakerContainer: {
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    flexDirection: 'row'
+  },
+
+  image: {
+    width: IMAGE_SIZE,
+    height: IMAGE_SIZE,
+    borderRadius: IMAGE_SIZE/2,
+    backgroundColor: "grey",
+    marginRight: 10,
+  },
+
+  speakerName: {
+    color: 'purple',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+
+  speakerBio: {
+    fontSize: 13,
+  },
+
 });
